ad11cde765dcdccd5d39b90db2b6aa57c4e34779
Aydrian Howard <aydrian@gmail.com>
2018-06-02 10:15:07 -0400

    Cart page start

--- webui/src/content/Cart.js
+++ webui/src/content/Cart.js
@@ -6,12 +6,51 @@ class Cart extends Component {
   }
 
   render() {
+    const products = this.props.cart.products
     return (
-      <div>
+      <div className="container">
         <h2>Cart</h2>
+        <div className="card">
+          <div className="card-body">
+            {products.length > 0 && (
+              <div className="container">
+                <div className="row">
+                  <div className="col" />
+                  <div className="col">Name</div>
+                  <div className="col">Quantity</div>
+                  <div className="col">Price</div>
+                  <div className="col" />
+                </div>
+                {products.map(product => (
+                  <CartItem key={product.id} product={product} />
+                ))}
+              </div>
+            )}
+            {products.length === 0 && (
+              <div className="alert alert-info">Cart is empty</div>
+            )}
+            <div className="cart__total">
+              Total: ${this.props.cart.totalPrice.toFixed(2)}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+const CartItem = ({ product, handleDeleteFromCart }) => (
+  <div className="row">
+    <div className="col">
+      <img src={product.image.thumb} alt={product.name} />
+    </div>
+    <div className="col">{product.name}</div>
+    <div className="col">{product.quantity}</div>
+    <div className="col">${product.price.toFixed(2)}</div>
+    <div className="col">
+      <button className="btn btn-danger btn-xs">X</button>
+    </div>
+  </div>
+)
 
 export default Cart
