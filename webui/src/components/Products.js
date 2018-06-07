20371ce3ad8fc4df62ff90d8de3011fbce8815d2
Aydrian Howard <aydrian@gmail.com>
2018-06-07 11:47:13 -0400

    fixed card deck layout

--- webui/src/components/Products.js
+++ webui/src/components/Products.js
@@ -4,15 +4,48 @@ import { USDFormatter } from '../helpers'
 
 const ProductDeck = ({ products, handleAddToCart }) => {
   return (
-    <div className={'card-deck w-75'}>
-      {products.map(product => {
-        return (
+    <div className={'card-deck'}>
+      {products.map((product, i) => {
+        let elms = [
           <ProductCard
             key={product.id}
             product={product}
             handleAddToCart={handleAddToCart}
           />
-        )
+        ]
+        if ((i + 1) % 2 === 0) {
+          elms.push(
+            <div
+              key={`2-spacer-${i + 1}`}
+              className="w-100 d-none d-sm-block d-md-none"
+            />
+          )
+        }
+        if ((i + 1) % 3 === 0) {
+          elms.push(
+            <div
+              key={`3-spacer-${i + 1}`}
+              className="w-100 d-none d-md-block d-lg-none"
+            />
+          )
+        }
+        if ((i + 1) % 4 === 0) {
+          elms.push(
+            <div
+              key={`4-spacer-${i + 1}`}
+              className="w-100 d-none d-lg-block d-xl-none"
+            />
+          )
+        }
+        if ((i + 1) % 5 === 0) {
+          elms.push(
+            <div
+              key={`5-spacer-${i + 1}`}
+              className="w-100 d-none d-xl-block"
+            />
+          )
+        }
+        return elms
       })}
     </div>
   )
@@ -20,12 +53,16 @@ const ProductDeck = ({ products, handleAddToCart }) => {
 
 const ProductCard = ({ product, handleAddToCart }) => {
   return (
-    <div className={'card col-md-4'}>
-      <img className={'card-img-top'} src={product.image.large} alt={''} />
-      <div className={'card-body'}>
-        <h5 className={'card-title'}>
+    <div className="card mb-4" style={{ maxWidth: '15rem' }}>
+      <img
+        className="card-img-top img-fluid"
+        src={product.image.large}
+        alt={product.name}
+      />
+      <div className="card-body">
+        <h4 className="card-title">
           <Link to={`/products/${product.id}`}>{product.name}</Link>
-        </h5>
+        </h4>
         <p className={'card-text'}>
           <small>{product.overview}</small>
         </p>
