1e62c18c49836197274d8143d1744e8949c2a673
Aydrian Howard <aydrian@gmail.com>
2018-06-07 12:31:58 -0400

    Cleaned up Cart page for Mobile

--- webui/src/content/Cart.js
+++ webui/src/content/Cart.js
@@ -12,17 +12,17 @@ const Cart = ({ cart, handleUpdateCartItem, handleCheckout, history }) => {
           {products.length > 0 && (
             <div className="container">
               <div className="row">
-                <div className="col col-2" />
-                <div className="col col-6">
+                <div className="col-2 col-md-2 d-none d-sm-block" />
+                <div className="col-6 col-md-6 d-none d-sm-block">
                   <strong>Name</strong>
                 </div>
-                <div className="col col-2">
+                <div className="col-2 col-md-2 d-none d-sm-block">
                   <strong>Quantity</strong>
                 </div>
-                <div className="col col-1">
+                <div className="col-1 col-md-1 d-none d-sm-block">
                   <strong>Price</strong>
                 </div>
-                <div className="col col-1" />
+                <div className="col-1 col-md-1 d-none d-sm-block" />
               </div>
               {products.map(product => (
                 <CartItem
@@ -60,24 +60,24 @@ const Cart = ({ cart, handleUpdateCartItem, handleCheckout, history }) => {
 
 const CartItem = ({ product, handleUpdateCartItem }) => (
   <div className="row">
-    <div className="col col-2">
+    <div className="col-md-2 col-6">
       <img src={product.image.thumb} alt={product.name} />
     </div>
-    <div className="col col-6">
+    <div className="col-md-6 col-6">
       <Link to={`/products/${product.id}`}>{product.name}</Link>
     </div>
-    <div className="col col-2">
+    <div className="col-md-2 col-4">
       <input
         type="number"
         value={product.quantity}
-        className="form-control col-5"
+        className="form-control col-md-5"
         onChange={e =>
           handleUpdateCartItem(product.id, parseInt(e.target.value, 10))
         }
       />
     </div>
-    <div className="col col-1">{USDFormatter.format(product.price)}</div>
-    <div className="col col-1">
+    <div className="col-md-1 col-4">{USDFormatter.format(product.price)}</div>
+    <div className="col-md-1 col-4">
       <button
         className="btn btn-danger btn-sm"
         onClick={() => handleUpdateCartItem(product.id)}
