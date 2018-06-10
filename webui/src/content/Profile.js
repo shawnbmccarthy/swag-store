00c0c5009cc7208f43a201cb3966858684c51b46
Michael Lynn <merlynn@gmail.com>
2018-06-10 09:59:48 -0400

    Update Profile page to include proper order item total and order total (#4)

--- webui/src/content/Profile.js
+++ webui/src/content/Profile.js
@@ -5,21 +5,41 @@ import '../components/Profile.css'
 
 const OrderItem = ({ product }) => (
   <div className="row">
-    <div className="col-md-2 col-6">
-      <img src={product.image.thumb} alt={product.name} />
+    <div className="col-md-6 col-6">
+      
     </div>
-    <div className="col-md-4 col-4">
-      <Link to={`/products/${product.id}`}>{product.name}</Link>
+    <div className="col-md-2 col-2">
+      <strong>Qty</strong>
     </div>
-    <div className="col-md-2 col-4">
-      <input
-        type="number"
-        value={product.quantity}
-        className="form-control col-md-12"
-      />
+    <div className="col-md-2 col-2">
+    <strong>Price</strong>
     </div>
-    <div className="col-md-4 col-4">{USDFormatter.format(product.price)}</div>
+    <div className="col-md-2 col-2">
+    <strong>Total</strong>
+    </div>
+    <div className="col-md-6 col-6">
+      <img src={product.image.thumb} alt={product.name} /><br />
+
+      <Link to={`/products/${product.id}`}><small>{product.name}</small></Link>
+    </div>
+    <div className="col-md-2 col-2">
+      {product.quantity}
+    </div>
+    <div className="col-md-2 col-2">{USDFormatter.format(product.price)} </div>
+    <div className="col-md-2 col-2">{USDFormatter.format(product.price*product.quantity)} </div>
   </div>
+)
+
+const OrderTotal = ({ products }) => (
+  <div className="col-12 col-md-12 order-total">
+    <div className="text-right"><strong>Order Total:</strong>&nbsp;
+      {USDFormatter.format(products.reduce((sum, i) => (
+        sum += i.quantity * i.price
+      ), 0))}
+    </div>
+    <hr />
+  </div>
+
 )
 
 export default class ProfilePage extends Component {
@@ -27,12 +47,14 @@ export default class ProfilePage extends Component {
     super(props)
     this.state = {}
   }
+  ordersTotal(orders) {
+
+  }
   render() {
     const userInfo = this.props.userInfo
     return (
-      <div className="container page">
-        <h2>Profile</h2>
-
+      <div className="container">
+        <br/><h2>Profile</h2><br/>
         <div className="col-xs-12 col-sm-9">
           <h4 className="blue">
             <span className="middle">
@@ -95,49 +117,49 @@ export default class ProfilePage extends Component {
 
           <div className="hr hr-8 dotted" />
         </div>
-        <h2>Orders</h2>
+        <br/><h2>My Orders</h2><br/>
         <div className="card">
           <div className="card-body">
             {userInfo.orders.length > 0 && (
               <div className="container">
                 <div className="row">
-                  <div className="col-1 col-md-1 d-none d-sm-block" />
                   <div className="col-2 col-md-2 d-none d-sm-block">
-                    <strong>Order Date</strong>
+                    <strong>## Order Date</strong>
                   </div>
                   <div className="col-2 col-md-2 d-none d-sm-block">
                     <strong>Status</strong>
                   </div>
-                  <div className="col-7 col-md-7 d-none d-sm-block">
+                  <div className="col-6 col-md-6 d-none d-sm-block">
                     <strong>Products</strong>
                   </div>
                 </div>
                 {userInfo.orders.map((order, i) => {
                   return (
-                    <div className="row">
-                      <div className="col-1 col-md-1 d-none d-sm-block">
-                        {i + 1}
-                      </div>
+                    <div className="row" key={order.id.toString()}>
                       <div className="col-2 col-md-2 d-none d-sm-block">
-                        {DateFormatter.format(order.date_created)}
+                      {i + 1}&nbsp;{DateFormatter.format(order.date_created)}
                       </div>
                       <div className="col-2 col-md-2 d-none d-sm-block">
                         {order.status}
                       </div>
-                      <div className="col-7 col-md-7 d-none d-sm-block">
+                      <div className="col-8 col-md-8 d-none d-sm-block">
                         {order.products.map(product => (
                           <OrderItem key={product.id} product={product} />
                         ))}
                       </div>
+                      
+                      <div className="row col-md-12">
+                          <OrderTotal 
+                            products={order.products} 
+                            key={order.id.toString()}
+                          />
+                      </div>
                     </div>
                   )
-                })}
-                <div className="row">
-                  <div className="order-total">
-                    <strong>Total:</strong>{' '}
-                    {USDFormatter.format(userInfo.orders.total)}
-                  </div>
-                </div>
+                }
+                
+                )
+              }
               </div>
             )}
             {userInfo.orders.length === 0 && (
